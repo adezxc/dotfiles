5aafcab0e46805bbfa1e5169b0f88a92aba07cf5
adezxc <adam@jasinski.lt>
2025-07-19 23:24:14 +0300

    Add vim-fugitive and vim-rhubarb

---
+++ nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'tpope/vim-fugitive',
+        opts = {},
+        config = function ()
+        end
+    },
+    {
+        'tpope/vim-rhubarb',
+        opts = {},
+        config = function ()
+        end
+    },
+}
